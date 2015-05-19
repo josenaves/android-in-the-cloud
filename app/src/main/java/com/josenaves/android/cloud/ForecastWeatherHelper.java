c8d17fad4f4ebb68b479f99ff2a7c9f7a1846617
Neto <josenaves@gmail.com>
2015-05-18 22:39:14 -0300

    Implementacao utilizando OkHttp

--- app/src/main/java/com/josenaves/android/cloud/ForecastWeatherHelper.java
+++ app/src/main/java/com/josenaves/android/cloud/ForecastWeatherHelper.java
@@ -2,12 +2,10 @@ package com.josenaves.android.cloud;
 
 import android.util.Log;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,22 +25,27 @@ public class ForecastWeatherHelper {
      * @return String contendo o JSON com as informações
      */
     public List<ForecastWeather> getWeather() {
-        String jsonResult = null;
-        HttpURLConnection urlConnection = null;
-
         List<ForecastWeather> forecastList = new ArrayList<>();
-        ForecastWeather forecastWeather = null;
+        ForecastWeather forecastWeather;
+        String jsonResult;
 
         try {
             URL url = new URL(API_ENDPOINT);
 
-            urlConnection = (HttpURLConnection)url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
+            // 1 - Cria um novo cliente OkHttp
+            OkHttpClient okHttpClient = new OkHttpClient();
 
-            jsonResult = readFromTheCloud(urlConnection.getInputStream());
+            // 2 - Constroe uma nova requisição com a URL
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            // 3 - Executa e obtém a resposta
+            Response response = okHttpClient.newCall(request).execute();
+
+            // 4 - Pega o corpo da resposta que contém o JSON
+            jsonResult = response.body().string();
             Log.d(TAG, "result = " + jsonResult);
-
 
             for (int i = 0; i < 3; i++) {
                 forecastWeather = ForecastWeather.getFromJson(jsonResult, i);
@@ -50,44 +53,10 @@ public class ForecastWeatherHelper {
                 Log.d(TAG, "ForecastWeather = " + forecastWeather);
             }
 
-        } catch (MalformedURLException e) {
-            Log.e(TAG, "Error ", e);
-
-        } catch (IOException e) {
-            Log.e(TAG, "Error ", e);
         } catch (Exception e) {
             Log.e(TAG, "Error ", e);
         }
-        finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-        }
+
         return forecastList;
-    }
-
-    // Lê um InputStream e converte para uma String.
-    private String readFromTheCloud(InputStream stream) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        StringBuilder sb = new StringBuilder();
-        String line;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "Error ", e);
-        }
-        finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (final IOException e) {
-                    Log.e(TAG, "Erro ao fechar o stream", e);
-                }
-            }
-        }
-
-        return sb.toString();
     }
 }
